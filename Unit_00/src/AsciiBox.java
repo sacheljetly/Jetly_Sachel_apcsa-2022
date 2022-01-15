9ff429a20d59878b0d953b544ef387b12e209e0e
sachelJ <loveskating22@gmail.com>
2022-01-14 23:36:54 -0800

    changed

---
+++ Unit_00/src/AsciiBox.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -Sachel Jetly
+//Date -01/14/22
+//Class -apcsa p1
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Sachel Jetly \t  01/14/22 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
---
+++ Unit_00/src/Variables.java
@@ -0,0 +1,67 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -Sachel Jetly
+//Date - 01/14/22
+//Class - apcsa p1
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		int intTwo = 7;
+		short shortSix = 1;
+		long longTen = 1000; 
+
+		//decimal variables
+		float floatThree = 10.5f;
+		double doubleFive = 103.443;
+		
+		
+
+		//other integer types
+		char charSeven = 'A';
+		
+
+		//other types
+		boolean boolEight = true;
+		String stringNine = "cat";
+		
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.print( byteOne + ",");
+		System.out.print( intTwo + ",");
+		System.out.print( shortSix + ",");
+		System.out.print( longTen + ",");
+		System.out.print( floatThree + ",");
+		System.out.print( charSeven + ",");
+		System.out.print( boolEight + ",");
+		System.out.print( stringNine + ",");
+		System.out.println( doubleFive + ",");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
