e408474465a7eee5803319a8b656262f19bda7fd
sachelJ <loveskating22@gmail.com>
2022-01-17 15:34:08 -0800

    changes 1/14

---
+++ Unit_01/src/StarsAndStripes.java
@@ -0,0 +1,73 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+
+public class StarsAndStripes
+{
+	
+	public static final String TEXT_RESET = "\u001B[0m";
+	public static final String TEXT_BLACK = "\u001B[30m";
+	public static final String TEXT_RED = "\u001B[31m";
+	public static final String TEXT_GREEN = "\u001B[32m";
+	public static final String TEXT_YELLOW = "\u001B[33m";
+	public static final String TEXT_BLUE = "\u001B[34m";
+	public static final String TEXT_PURPLE = "\u001B[35m";
+	public static final String TEXT_CYAN = "\u001B[36m";
+	public static final String TEXT_WHITE = "\u001B[37m";
+
+	
+	
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+      
+   }
+
+   public void printTwentyStars()
+   { 
+	   out.println("********************");
+	   
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("|--------------------|");
+	
+   }
+   
+
+   public void printTwoBlankLines()
+   {out.println("                    \n                    ");
+   }
+   
+   public void printASmallBox()
+   { out.println("------");
+   out.println("|     |\n|     |\n|     |");
+   out.println("------");
+   
+   }
+ 
+   public void printABigBox()
+   { out.println("--------------------------");
+   out.println("|                        |\n|                        |\n|                        |\n|                        |\n|                        |\n|                        |\n|                        |\n|                        |");
+   out.println("--------------------------");
+   }   
+   
+   public void printFlagPTone ()
+   {
+	   out.println("\u001B[34m|********\u001B[31m --------------------------|");
+   }
+   public void printFlagPTtwo ()
+   {
+	   out.println("\u001B[31m|-----------------------------------|");
+   }
+}
+
---
+++ Unit_01/src/StarsRunner.java
@@ -0,0 +1,42 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes flag = new StarsAndStripes();
+      
+      //call the methods needed to make the patterns on the word document
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwoBlankLines();
+      flag.printTwentyStars();
+      flag.printTwentyStars();
+      flag.printTwentyDashes();
+      flag.printTwentyStars();
+      flag.printTwentyStars();
+      flag.printASmallBox();
+      flag.printTwoBlankLines();
+      flag.printABigBox();
+      
+      
+      for (int i=0; i <4;i++) {
+    	  flag.printFlagPTone();
+      }
+      for (int i=0; i<9; i++) {
+    	  flag.printFlagPTtwo();
+      }
+      
+
+   }
+}
