c24f060c4df35b9d219e12a37d9ee6ab3a34efc2
sachelJ <loveskating22@gmail.com>
2022-01-20 20:27:56 -0800

    Changes 1/20

--- Unit_03/src/Distance.java
+++ Unit_03/src/Distance.java
@@ -17,23 +17,43 @@ public class Distance
 
 	public Distance()
 	{
+		
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
-	}
+		
+		xOne = x1;
+		yOne = y1; 
+		xTwo = x2;
+		yTwo = y2;
+	} 
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1; 
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void calcDistance()
 	{
+		//first part of the equation
+		int i = (xTwo-xOne);
+		double squareOne = Math.pow(i, 2);
+		
+		//second part of the equation
+		int f = (yTwo-yOne);
+		double squareTwo = Math.pow(f, 2);
+		
+		distance = Math.sqrt(squareOne + squareTwo);
+		
 	}
 	
 	public double getDistance()
 	{
-		return 0.0;
+		return distance;
 	}
 	
 	public void print()
@@ -46,4 +66,6 @@ public class Distance
 	{
 		return "";
 	}
+
+	
 }
--- Unit_03/src/DistanceRunner.java
+++ Unit_03/src/DistanceRunner.java
@@ -14,7 +14,31 @@ public class DistanceRunner
 {
 	public static void main( String[] args )
 	{
-		//add test cases	
+	
+		Distance cat = new Distance(1,2,3,4); 
+		
+		int x1;
+		int y1;
+		int x2;
+		int y2;
+		
 			
+	//User input for values	
+		Scanner keyboard = new Scanner(System.in);
+		out.print("Input a value for x1 :: ");
+		x1= keyboard.nextInt();
+		out.print("Input a value for y1 :: ");
+		 y1 = keyboard.nextInt();
+		 out.print("Input a value for x2 :: ");
+		 x2= keyboard.nextInt();
+		 out.print("Input a value for y2 :: ");
+		 y2 = keyboard.nextInt();
+	//Setting values based on input	 
+		cat.setCoordinates(x1, y1, x2, y2);
+		
+		cat.calcDistance();
+		out.print("Distance :: ");
+	System.out.printf("%.3f\n", cat.getDistance());	
+	
 	}
 }
