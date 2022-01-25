ddf59071d13b23f82b53c022de42fdbfe070100e
sachelJ <loveskating22@gmail.com>
2022-01-24 19:24:30 -0800

    Changes Unit 4

--- Unit_04/src/NumberVerifyRunner.java
+++ Unit_04/src/NumberVerifyRunner.java
@@ -16,7 +16,15 @@ public class NumberVerifyRunner
 		//add in input
 		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
 		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		System.out.println("10000 is even :: " + NumberVerify.isEven(10000));
+		System.out.println("12345 is odd :: " + NumberVerify.isOdd(12345));
+		System.out.println("12345 is even :: " + NumberVerify.isEven(12345));
+		System.out.println("1 is even :: " + NumberVerify.isEven(1));
+		System.out.println("1 is odd :: " + NumberVerify.isOdd(1));
+		System.out.println("17 is even :: " + NumberVerify.isEven(17));
+		System.out.println("17 is odd :: " + NumberVerify.isOdd(17));
 		
-		//add in more test cases
+		
+
 	}
 }
