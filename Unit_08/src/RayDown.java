5dc26723a36c4ed0a2ab803052736d55b150a07a
sachelJ <loveskating22@gmail.com>
2022-02-10 23:49:40 -0800

    changes

--- Unit_08/src/RayDown.java
+++ Unit_08/src/RayDown.java
@@ -3,25 +3,38 @@
 //Name -
 //Date -
 
+
+
 public class RayDown
 {
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{ 
-		boolean output = false; 
-		for (int i =0; i<numArray.length-1; i++)
+		
+		if (numArray.length>1)
+		{
+		for (int i =0; i<numArray.length && (i+1<numArray.length); i++)
 		{
 			if (numArray[i]> numArray[i+1])
 			{
-				output = false; 
+				return true; 
+				
 			}
+
+			else	
+		
+			return false; 
 			
-			output= true; 
-			
+						
 		}
-		return output;
+		}
+		else  
+			return true; 
+	
+		return false;
 		
 		
-	}	
+		
+	}
 }
