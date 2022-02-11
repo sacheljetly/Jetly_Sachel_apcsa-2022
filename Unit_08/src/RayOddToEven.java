2445a8dd3cbc54a6d0b0844811daa65892c13983
sachelJ <loveskating22@gmail.com>
2022-02-10 23:18:18 -0800

    changes

--- Unit_08/src/RayOddToEven.java
+++ Unit_08/src/RayOddToEven.java
@@ -5,8 +5,55 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	
+	// Write a program that will search an array to find the first odd number. 
+	//If an odd number is found, then find the first even number following the odd
+	//number. Return the distance between the first odd number and the FIRST even
+	//number. Return -1 if no odd numbers are found or there are no even numbers 
+	//following an odd number.
+
+	public static int go(int[] ray)
 	{
-		return false;
-	}
-}
+	    int firstOdd=0; 
+		int firstEven = 0; 
+		int count =0; 
+		
+		for (int i = 0; i< ray.length; i++)
+		{
+				if ( ray[i]%2==1)
+				{
+					for (int d = i; d<ray.length; d++)
+					{
+						if (ray[d]%2==0)
+						{
+							return d-i; 
+						}
+					//firstOdd = i; 
+				}
+			break; 
+			
+		/*	for (int d = i; d<ray.length; d++)
+			{
+				if (ray[d]%2==0)
+				{
+					firstEven = d;
+				break;
+				}
+				else
+					count = 0; 
+			}
+			count = firstEven-firstOdd; 
+			break; 
+				
+		}
+		if (count ==0)
+		{ 
+			count = -1; 
+	*/	
+				}}
+return -1; 
+		
+		
+	
+					
+}}
