9caa54f91f7a12d34a6ab636ea1d8c49a55e64c5
sachelJ <loveskating22@gmail.com>
2022-02-10 21:36:20 -0800

    changes

--- Unit_08/src/RaySumLast.java
+++ Unit_08/src/RaySumLast.java
@@ -5,8 +5,29 @@
 
 public class RaySumLast
 {
+	// Write a program that will loop through an array and
+	//sum up all values > than the last value in the array.  
+	//If there are no values greater than the last value in the array or
+	//if the array is empty, return -1.
+
 	public static int go(int[] ray)
 	{
-		return 0;
+		int rayCount= 0; 
+		for (int i =0; i<ray.length; i++)
+		{
+			if (ray[i]> ray[ray.length-1])
+			{
+				rayCount+=ray[i]; 
+				
+			}
+			else rayCount += 0; 
+			
+		}
+		if (rayCount<=0)
+			rayCount = -1; 
+		System.out.println(rayCount); 
+		return rayCount;
+		
 	}
+	
 }
--- Unit_08/src/SumLastRunner.java
+++ Unit_08/src/SumLastRunner.java
@@ -6,6 +6,20 @@
 public class SumLastRunner
 {
 	public static void main( String args[] )
-	{											
+	{				
+		RaySumLast test = new RaySumLast(); 
+		test.go(new int[] {-99,1,2,3,4,5,6,7,8,9,10,5});
+		test.go(new int[] {10,9,8,7,6,5,4,3,2,1,-99});
+		test.go(new int[] {10,20,30,40,50,-11818,40,30,20,10});
+		test.go(new int[] {32767});
+		test.go(new int[] {255,255});
+		test.go(new int[] {9,10,-88,100,-555,2});
+		test.go(new int[] {10,10,10,11,456});
+		test.go(new int[] {-111,1,2,3,9,11,20,1});
+		test.go(new int[] {9,8,7,6,5,4,3,2,0,-2,6});
+		test.go(new int[] {12,15,18,21,23,1000});
+		test.go(new int[] {250,19,17,15,13,11,10,9,6,3,2,1,0});
+		test.go	(new int[] {9,10,-8,10000,-5000,-3000});
+
 	}
 }
