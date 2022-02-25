1f85b4213fa66f86ed9973d61e37b20626295eb8
sachelJ <loveskating22@gmail.com>
2022-02-24 20:33:17 -0800

    changes

--- Unit_09-ArrayList/src/ListSumFirst.java
+++ Unit_09-ArrayList/src/ListSumFirst.java
@@ -10,6 +10,22 @@ public class ListSumFirst
 {
 	public static int go(List<Integer> ray)
 	{
-		return 0;
+		int sum = 0; 
+		if (ray.size()==1)
+		
+	return -1; 
+		
+		
+		
+		for (int i=0; i<ray.size(); i++)
+		{
+			if (ray.get(i)> ray.get(0))
+				sum += ray.get(i);
+		}
+
+if (sum==0)
+	sum = -1; 
+
+return sum; 
 	}
 }
