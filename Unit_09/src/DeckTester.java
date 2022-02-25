187e0c28541746cc3e293033ccb2e976d188c468
sachelJ <loveskating22@gmail.com>
2022-02-24 23:34:49 -0800

    changes

--- Unit_09/src/DeckTester.java
+++ Unit_09/src/DeckTester.java
@@ -16,14 +16,14 @@ public class DeckTester {
 		
 		Deck testDeck = new Deck(rankk ,suitt, PV); 
 		
-		
+		/*	
 		System.out.println("What is the deck size:: " + testDeck.size());
 		System.out.println("Is the deck empty or not ::" + testDeck.isEmpty());
 		testDeck.shuffle();
 		testDeck.deal();
 		System.out.print("\n");
 		System.out.println(testDeck.toString());
-		
+	*/	
 		
 		System.out.println("Shuffle Method");
 		String[] RANK1 = new String[52]; 
@@ -34,12 +34,47 @@ public class DeckTester {
 		
 		
 		
-		Deck testDeck1 = new Deck(RANK1 ,SUIT1, POINTVALUE1);
 		
-		testDeck1.shuffle();
-	System.out.print(	testDeck1.toString()); 
 		
+		
+		 for (int i = 0; i < 52; i++) {
+	            String rank = "";
+	            String suit = "";
+	            if ( (i+1) % 13 == 11 ) {
+	                rank = "Jack";
+	            }
+	            else if ( (i+1) % 13 == 12 ) {
+	                rank = "Queen";
+	            }
+	            else if ( (i+1) % 13 == 0 ) {
+	                rank = "King";
+	            }
+	            else if ( (i+1) % 13 == 1 ) {
+	                rank = "Ace";
+	            }
+	            else {
+	                rank = Integer.toString((i + 1) % 13);
+	            }
+	            
+	            if (i < 13) {
+	                suit = "Hearts";
+	            }
+	            else if (i < 26) {
+	                suit = "Diamonds";
+	            }
+	            else if (i < 39) { 
+	                suit = "Clubs";
+	            }
+	            else {
+	                suit = "Spades";
+	            }
+	            
+	          RANK1[i] = rank;
+	            SUIT1[i] = suit;
+	            POINTVALUE1[i] = ((i+1) % 13);
+	            Deck testDeck1 = new Deck(RANK1 ,SUIT1, POINTVALUE1);
+	            System.out.print(testDeck1);
 		
 		
 	}
-}
+}}
