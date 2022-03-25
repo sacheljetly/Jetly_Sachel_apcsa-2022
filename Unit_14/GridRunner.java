3eb0ac273f2f1065de7ce5081248a23c0b00741a
sachelJ <loveskating22@gmail.com>
2022-03-24 20:25:55 -0700

    update

--- Unit_14/GridRunner.java
+++ Unit_14/GridRunner.java
@@ -11,5 +11,11 @@ public class GridRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		
+		String [] vals = { "a" , "b" , "c" , "7", "9", "x", "2"}; 
+		Grid test = new Grid(20, 20, vals); 
+		out.println(test);
+		}
 	}
 }
+
