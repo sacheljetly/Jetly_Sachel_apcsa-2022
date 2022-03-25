c7227af5011af780753402a8de3b97540d3aef88
sachelJ <loveskating22@gmail.com>
2022-03-24 20:35:15 -0700

    v

--- Unit_14_new/src/MatrixCount1.java
+++ Unit_14_new/src/MatrixCount1.java
@@ -3,6 +3,9 @@
 //Name -
 
 import java.util.*;
+
+import static java.lang.System.out;
+
 import java.io.*; 
 
 public class MatrixCount1
@@ -18,4 +21,66 @@ public class MatrixCount1
 		//add code
 		return 0;
     }
+}
+
+
+private String[][] grid;
+
+//load vals into the rows x cols grid randomly
+	public Grid(int rows, int cols, String[] vals)
+	{
+		Grid = new String[r][c];
+		
+		for (r = 0; r < Grid.length; r++)
+		{n
+			for (c = 0; c<Grid[rows].length; c++)
+			{
+				Grid[r][c] = vals[ (int) (Math.random()*5)];
+			}
+		}
+		for (int i =0; i<r; i++)
+		{
+			for (int j=0; j<c; j++)
+			{
+				System.out.print(" " + Grid[r][c]);
+			}
+			System.out.println();
+		}
+	}
+
+	//find out which of the vals occurs the most
+	public String findMax(String[] vals)
+	{
+		return "nothing yet";
+	}
+
+	//returns a count of how many times val occurs in the matrix
+	private int countVals( String val )
+	{
+		int count =0; 
+		for (int i = 0; i<Grid.length; i++)
+		{
+			for (int j = 0; j<Grid[0].length; j++)
+			{
+			if (grid[i][j] ==val)
+				count++; 
+			}
+		}
+	}
+
+	//display the grid
+	public String toString()
+	{
+		String output=" is the most common value";
+		return output;
+	}
+	
+	
+	
+	
+
+	String [] vals = { "a" , "b" , "c" , "7", "9", "x", "2"}; 
+	Grid test = new Grid(20, 20, vals); 
+	out.println(test);
+	}
 }
